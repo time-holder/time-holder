94a869625ced96cee6d1a6b7f371013b69c365c7
timeholder <timeholder.xyz@gmail.com>
2024-05-03 11:34:33 +0900

    perf: update hardhat.config

--- contracts/TIME.sol
+++ contracts/TIME.sol
@@ -5,6 +5,8 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TIME is ERC20 {
   constructor() ERC20("TIME", "TIME") {
+    // Will never be minted again.
+    // Symbolizing a millennium, each token represents one second.
     _mint(msg.sender, 31_536_000_000 * 10 ** decimals());
   }
 }
